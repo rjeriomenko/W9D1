d2792f93ca815215d3379fe6500a37197013e262
rokasj <rokas@jerio.me>
2023-05-01 15:07:05 -0400

    Finish Phase 1

---
+++ asteroids/src/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Webpack is working!")
+
+    const canvas = document.getElementById("game-canvas");
+    const ctx = canvas.getContext("2d");
+});
